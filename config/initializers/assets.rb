663eaf214abb70dc8c0ed27c47d4603e5d576f03
emanuel.perez.astorga@gmail.com <emanuel.perez.astorga@gmail.com>
2018-10-03 21:57:15 -0300

    8

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,8 +10,11 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( *.jpg )
+Rails.application.config.assets.precompile += %w( *.png )
